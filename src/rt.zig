2f643eebe0f59b30ba5fa27a829f9f2f37a801d5
CutieDeng <CutieDeng@iCloud.com>
2023-01-31 05:19:31 +0800

    Fulfill the comments of 'rt.zig'.

--- src/rt.zig
+++ src/rt.zig
@@ -1,3 +1,8 @@
+//! The zig runtime of the os. 
+//! In this module, the start entry `_start` is defined, and it would jump to the function `main`, which defined in root project ~ 
+//! Also, the stack part is defined here, with the size 4KiB. 
+//! Some symbols defined in linker script are used here to give a better initialize support. 
+
 comptime {
     asm (
         \\.section .text.my_entry 
@@ -6,42 +11,45 @@ comptime {
         \\  la sp, boot_stack_top
         \\  call main 
         \\.section .bss.stack 
-        \\.globl boot_stack
         \\boot_stack:
-        \\.space 4096 * 16 * 16
+        \\.space 4096
         \\.globl boot_stack_top
         \\boot_stack_top: 
     ); 
 }
 
+/// This is the symbol 'sbss' defined in linker.ld, start of the segment '.bss'. 
 extern const sbss : u8; 
+/// Defined in linker.ld, end of the segment '.bss'. 
 extern const ebss : u8; 
 
-pub fn emptyBss() void {
+/// Make the content of segment '.bss' all zero; zero initialized. 
+fn emptyBss() callconv(.Inline) void {
     const start_ptr : [*]u8 = @ptrCast([*]u8, &sbss); 
     const length = @ptrToInt(&ebss) - @ptrToInt(&sbss); 
     @memset(start_ptr, 0, length);
 }
 
+/// init fn array: define the init functions orderly, call them to initialize the runtime. 
+/// - emptyBss: this fn flush the segment '.bss' . 
+pub const init = [_] *const fn () callconv(.Inline) void { emptyBss, }; 
+
+// create some symbols and put the program here. 
 comptime {
     asm( 
         \\.align 3
         \\.section .data 
-        \\.global app_numbers
         \\app_numbers: 
         \\.quad 2 
         \\.quad app0_start
+        \\.quad app0_end
         \\.quad app1_start
         \\.quad app1_end 
         \\.section .data 
-        \\.global app0_start
-        \\.global app0_end 
         \\app0_start: 
         \\.incbin "apps/hello/zig-out/bin/hello.bin"
         \\app0_end: 
         \\.section .data
-        \\.global app1_start
-        \\.global app1_end
         \\app1_start:
         \\.incbin "apps/view/zig-out/bin/view.bin"
         \\app1_end:
